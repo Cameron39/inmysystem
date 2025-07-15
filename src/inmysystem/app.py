d8cb9970d9b06c1256624821be05edf599541dc6
Cameron <cameron.39@gmail.com>
2025-07-14 20:47:38 -0500

    Removed background image

--- src/inmysystem/app.py
+++ src/inmysystem/app.py
@@ -4,7 +4,7 @@ In My System CSC 470 Final Project
 
 import toga
 from toga.style import Pack
-from toga.style.pack import COLUMN, ROW
+from toga.style.pack import COLUMN, ROW, CENTER, Pack
 from toga import ImageView, Image
 
 
@@ -16,17 +16,37 @@ class InMySystem(toga.App):
         We then create a main window (with a name matching the app), and
         show the main window.
         """
-        main_box = toga.Box()
-        #self.photo = toga.ImageView(height=1280, width=924, margin = 5)
-        # main_box.add(self.photo)
-        bck_image = toga.ImageView(toga.Image('./resources/background2.png'))
-        #bck_image.
-        main_box.add(bck_image)
+        main_box = toga.Box(
+            style=Pack(
+                margin=5,
+                align_items=CENTER,
+                direction=COLUMN,
+                width = 435,
+                height = 640,
+            )
+        )
+
+        add_button = toga.Button(
+            "Add Dose",
+            on_press= self.add_dose,
+            margin=5,
+        )
+        
+
+        # bck_image = toga.ImageView(toga.Image('./resources/background2.png'),
+        #                            style=Pack(flex=1,
+        #                            align_items=CENTER,
+        #                            width = 435,
+        #                            height = 640,)
+        #                            )
+        # main_box.add(bck_image)
 
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = main_box
         self.main_window.show()
 
+    def add_dose(self):
+        pass
 
 def main():
     return InMySystem()
